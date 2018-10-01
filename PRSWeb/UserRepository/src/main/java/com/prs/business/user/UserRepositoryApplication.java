ac9a4ea85c27ac2bb768dc39074a2a51691e27e1
Lance-Mendenhall <lancesrealm@gmail.com>
2018-10-01 08:55:40 -0400

    PRS Project

---
+++ PRSWeb/UserRepository/src/main/java/com/prs/business/user/UserRepositoryApplication.java
@@ -0,0 +1,12 @@
+package com.prs.business.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRepositoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRepositoryApplication.class, args);
+	}
+}
