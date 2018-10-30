484f7dbf58352e38612e9069e9d32c0490582884
Lance-Mendenhall <lancesrealm@gmail.com>
2018-10-30 16:26:45 -0400

    basic CRUD working

--- prsng/src/app/app-routing.module.ts
+++ prsng/src/app/app-routing.module.ts
@@ -5,13 +5,18 @@ import { UserListComponent } from './user/user-list/user-list.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
 import { UserCreateComponent } from './user/user-create/user-create.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { MenuComponent } from './menu/menu/menu.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path : '', redirectTo: '/users/list', pathMatch: 'full'},
   { path: 'users/list', component: UserListComponent},
-  { path: 'users/detail:id', component: UserDetailComponent},
+  { path: 'users/detail/:id', component: UserDetailComponent},
   { path: 'users/create', component: UserCreateComponent},
-  { path: 'users/edit:id', component: UserEditComponent},
+  { path: 'users/edit/:id', component: UserEditComponent},
+  { path: 'home', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
   { path: '**', component: UserListComponent }
 ];
 
